c4ae98acd170924b703d1f49d7761fd9d8c4eee0
Mihai Gaiseanu <Mihai Gaiseanu>
2017-02-06 19:55:46 +0200

    finished assignment 3 for angular course

--- ang-module3-solution/app.js
+++ ang-module3-solution/app.js
@@ -3,25 +3,73 @@
     angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
-   
+    .directive('foundItems', FoundItemsDirective)
+
+   // The found items directive
+   function FoundItemsDirective() {
+       var ddo = {
+            restrict: 'E',
+            templateUrl: '/snippets/foundMenuItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&',
+                showLoading: '=',
+                showTable: '='
+            },
+            controller: 'NarrowItDownController as dirCtrl',
+            bindToController: true
+       };
+       return ddo;
+   }
+
+   // The Controller
    NarrowItDownController.$inject = ['MenuSearchService'];
    function NarrowItDownController (MenuSearchService) {
        var ctrl = this;
 
-       this.items = MenuSearchService.getMatchedMenuItems;
+       ctrl.selectedItems = [];
+       ctrl.showLoading = false;
+       ctrl.showTable = false;
+       ctrl.searchTerm = "";
+       
+       ctrl.getItems = function() {
+           ctrl.showTable = false;
+           ctrl.showLoading = true;
+           var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+           promise.then(function successCallback(result){
+               ctrl.showLoading = false;
+               ctrl.selectedItems = result;
+               ctrl.showTable = true;
+           });
+       }; 
+       ctrl.removeItem = function(index) {
+           ctrl.selectedItems.splice(index, 1);
+       }
    }
 
+   // The Service
    MenuSearchService.$inject = ['$http'];
    function MenuSearchService ($http) {
        var service = this;
 
        service.getMatchedMenuItems = function (searchTerm) {
-           $http({
+           return $http({
                url : 'https://davids-restaurant.herokuapp.com/menu_items.json'
            }).then(function successCallback(response){
-               // do something with response.data
-               console.log(response.data);
-
+               // do something with response.data           
+               var foundItems = response.data.menu_items;
+               if (searchTerm && searchTerm != '')
+               {
+                   foundItems = foundItems.filter(function (element){
+                       return element.description.includes(searchTerm);
+                   });
+               }
+               else
+               {
+                   foundItems = [];
+               }
+                           
+               return foundItems;
            }, function errorCallback(response){
                // do something with the response
            });
