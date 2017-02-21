f515cf1e71d544d59a5141a62b667249c4f96192
mihai <mihai_gaiseanu@yahoo.com>
2017-02-21 23:32:55 +0200

    continued to work on module 5 assignment on angular module

--- ang-module5-solution/src/public/signup/signup.controller.js
+++ ang-module5-solution/src/public/signup/signup.controller.js
@@ -7,10 +7,21 @@
     SignupController.$inject = ['MenuService'];
     function SignupController(MenuService) {
         var $ctrl = this;
-
-
+        $ctrl.message = "";
         $ctrl.signUp = function() {
-            MenuService.getMenuItem($ctrl.favMenuItem);
+            MenuService.getMenuItem($ctrl.favMenuItem).then(function(response) {
+                console.log(response);
+                var message = "";
+                if (response)
+                    message = "Your information has been saved.";
+                else
+                {
+                    $ctrl.favMenuItem = "";
+                    message = "No such menu number exists";
+                }
+                $ctrl.message = message;
+
+            });
         }
     }
 
